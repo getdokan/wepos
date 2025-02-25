495343723ecf36d33dbde5e00a5c2743dcc1ffb3
Asad Nur <asaduzzamannur.dev@gmail.com>
2025-02-25 17:38:41 +0600

    fix: Unable to create order for exsting customers if billing email missing

--- assets/src/frontend/components/Home.vue
+++ assets/src/frontend/components/Home.vue
@@ -1092,6 +1092,10 @@ export default {
         },
 
         selectCustomer( customer ) {
+            if ( customer.email && ! customer.billing.email ) {
+                customer.billing.email = customer.email;
+            }
+
             this.$store.dispatch( 'Order/setCustomerAction', customer );
         },
         selectVariationProduct( product ) {
